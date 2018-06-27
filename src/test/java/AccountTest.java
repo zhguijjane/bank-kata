cf89693451646726a3868878cb7a42f18aba25eb
Zouhair Guijjane <zh.guijjane@gmail.com>
2018-06-27 20:21:00 +0200

    Initial commit

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,11 @@
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class AccountTest {
+
+    @Test
+    public void dummy() {
+        Assertions.assertThat(false).isFalse();
+    }
+
+}
