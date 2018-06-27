167c7b5443aea3b0f97e5decdb56b28b4b48a2b0
Zouhair Guijjane <zh.guijjane@gmail.com>
2018-06-27 22:09:40 +0200

    Refactor Deposit Test

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,12 +1,18 @@
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AccountTest {
 
+    private Account account;
+
+    @Before
+    public void setUp() {
+        account = new Account(0);
+    }
+
     @Test
     public void should_add_0_to_account() {
-        Account account = new Account(0);
-
         account.deposit(new Amount(0));
 
         Assertions.assertThat(account.getBalance()).isEqualTo(0);
@@ -14,8 +20,6 @@ public class AccountTest {
 
     @Test
     public void should_add_100_to_account() {
-        Account account = new Account(0);
-
         account.deposit(new Amount(100));
 
         Assertions.assertThat(account.getBalance()).isEqualTo(100);
